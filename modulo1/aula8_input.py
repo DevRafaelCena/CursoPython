9582c17fdb877fff9ee2567fe9cd2063e470fc3c
Rafael Cena <rafael.cena@hotmail.com>
2021-06-03 15:00:59 -0300

    aula8 input

---
+++ modulo1/aula8_input.py
@@ -0,0 +1,13 @@
+"""
+entrada de dados
+"""
+
+nome = input("Digite o seu nome:  ")
+idade = input("Qual a sua idade? ")
+ano = 2021 -int(idade)
+print(f'{nome} tem {idade} anos e nasceu em {ano}')
+
+numero_1 = int(input('Digite um numero'))
+numero_2 = int(input('Digite outro numero'))
+soma = numero_1 + numero_2
+print(f'a soma é {soma}')

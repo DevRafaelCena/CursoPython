c8078df2d44fcaa2504c3cb3c735c59743022cb0
Rafael Cena <rafael.cena@hotmail.com>
2021-06-03 13:15:10 -0300

    aula 2 função print

--- modulo1/aula1_comentarios.py
+++ modulo1/aula1_comentarios.py
@@ -1,8 +1,12 @@
 # comentario exemplo
+"""
+TESTE de comentario
 
+"""
 print("linha 1")
 print("linha 2")
 # Teste de comentario no codigo
 print("linha 3")  # Outro comentario
 print("linha 4")
+
 print("linha 5")
---
+++ modulo1/aula2_print.py
@@ -0,0 +1,8 @@
+print("Teste do print")
+print('Rafael', 'Cena')  # Retorna uma tupla?
+print('09', '12', '2016', sep='-')  # sep = separador
+print('09', '12', '2016', sep='-', end='!')  # end = valor final
+print('\n', "_"*10)
+
+
+

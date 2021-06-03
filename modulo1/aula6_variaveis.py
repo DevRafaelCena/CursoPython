ca478a9ae0e1122f96f86b46146b3af928498c88
Rafael Cena <rafael.cena@hotmail.com>
2021-06-03 14:33:20 -0300

    aula6 variaveis

---
+++ modulo1/aula6_variaveis.py
@@ -0,0 +1,16 @@
+"""
+Iniciar com letra, pode conter numeros, serpar _ , letras minusculas
+
+"""
+
+nome = "Rafael"
+idade = 30
+altura = 1.80
+e_maior = idade > 18
+
+print(nome, type(nome))
+
+print('Nome:', nome)
+print("Idade:", idade)
+print("Altura:", altura)
+print('É maior:', e_maior)

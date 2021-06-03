9753b4b78c66b05b672d4da2abe89fe047ae6b6b
Rafael Cena <rafael.cena@hotmail.com>
2021-06-03 14:13:29 -0300

    aula4 tipos primitivos

---
+++ modulo1/aula4_tipos_primitivos.py
@@ -0,0 +1,20 @@
+"""
+Tipos de dados
+
+str - string
+int - inteiro
+float - real/flutuante
+bool - booleano/logico
+
+"""
+print(type('luiz'))
+print(type(20))
+print(type(2.0))
+print(type(10==10))
+
+# convertendo tipos
+
+print(type('Luiz'), bool('Luiz'))
+print(type('10'), type(int('10')))
+
+

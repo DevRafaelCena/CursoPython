8f6cc8ba71a973fa9a84058a91030753b8e8acc3
Rafael Cena <rafael.cena@hotmail.com>
2021-06-03 14:20:25 -0300

    aula5 operadores

---
+++ modulo1/aula5_operadores.py
@@ -0,0 +1,20 @@
+"""
++,-,*,
+/ = divisao
+// = divisão inteiro
+** = potencia
+% = resto da divisao ,
+() = ordem
+
+"""
+
+print(10 * 10)  # multiplicação
+print(10 + 10)  # Adição
+print(10 - 5)  # subtração
+print(10 / 2)  # Divisao
+
+print(10 // 2)  # divisao com resultado inteiro
+print(5 ** 2)  # potencia
+print(25 % 3)  # resto
+print(2+2*2)
+print((2+2)*2)

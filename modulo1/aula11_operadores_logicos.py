aa2d5dfb7069172e69b5be19132f443b3ae8745e
Rafael Cena <rafael.cena@hotmail.com>
2021-06-03 17:00:16 -0300

    aula11 operadores logicos

---
+++ modulo1/aula11_operadores_logicos.py
@@ -0,0 +1,18 @@
+"""
+operadores logicos
+and , or , not
+in e not in
+"""
+a = 2
+b = 2
+c = 3
+print(a==b and b > c)
+
+print(not a!=b and not b>c)
+
+nome = "Rafae"
+
+if 'l' in nome:
+    print("tem letra l")
+else:
+    print("Não tem a letra")

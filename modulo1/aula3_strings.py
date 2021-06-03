ebb2bfa2d9b02ac48864ad3af5af15c7d0f7395f
Rafael Cena <rafael.cena@hotmail.com>
2021-06-03 13:40:04 -0300

    aula3 strings

---
+++ modulo1/aula3_strings.py
@@ -0,0 +1,13 @@
+"""
+str - string
+
+"""
+
+print('Alguma coisa')
+print("Outra coisa")
+print(12132232)
+print("teste 'um' aspas simples no meio do texto")
+print("teste \"um\" aspas simples no meio do texto")  # caracter de escape
+print(r"teste \"um\" aspas simples no meio do texto")  # é fiel ao texto
+
+

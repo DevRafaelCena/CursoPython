288275bd933ff06e8d490945823e42299e0ca37e
Rafael Cena <rafael.cena@hotmail.com>
2021-06-03 14:42:24 -0300

    aula7 formatando strings

---
+++ modulo1/aula7_formatando_strings.py
@@ -0,0 +1,7 @@
+nome = "Rafael Bezerra Cena"
+idade = 30
+altura = 1.80
+
+print(f'{nome} tem {idade} anos de idade e sua altura é {altura:.2f}')
+print('{} tem {} anos de idade e sua altura é {:.2f}'.format(nome, idade, altura))
+print('{} tem {} anos de idade e sua altura é {:.2f}'.format(nome, idade, altura))

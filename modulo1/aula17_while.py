51d66deae6ac75ef7e8e063a53872367995469ec
Rafael Cena <rafael.cena@hotmail.com>
2021-06-04 07:48:27 -0300

    aula17 while

---
+++ modulo1/aula17_while.py
@@ -0,0 +1,16 @@
+"""
+while em python
+
+while condicao:
+    pass
+
+pass = substitui o codigo para identar
+continue = pula a vez no laço
+break = termina o laço
+
+"""
+x = 0
+
+while x < 100:
+    print(f"Valor de X é : {x}")
+    x = x + 1

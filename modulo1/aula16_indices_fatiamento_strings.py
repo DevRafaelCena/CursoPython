91b6b764295a98784a97c4b827320d57aac1a1cf
Rafael Cena <rafael.cena@hotmail.com>
2021-06-03 21:55:10 -0300

    aula16 indices e fatiamento de strings

---
+++ modulo1/aula16_indices_fatiamento_strings.py
@@ -0,0 +1,19 @@
+"""
+Manipulando strings
+String indices
+Fatiamento de strings [inicio:fim:passo]
+Funções built-in len, abs, type, print , etc..
+essas funções podem ser usadas diretamente em cada tipo
+
+https://docs.python.org/3/library/functions.html
+https://docs.python.org/3/library/stdtypes.html
+"""
+
+texto = "Python forever"
+
+print(texto[3])
+print(texto[-5])
+print(texto[2:5])
+print(texto[:8])
+print(texto[:-8])
+print(texto[7:])
